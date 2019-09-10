9fa31b35e9e2c6f99fb5c836fcfae7a6ee2f29f4
Nick Maludy <nmaludy@gmail.com>
2019-09-10 15:18:10 -0400

    Don't do cache update for windows, it's redundant and takes forever. Added a check_online plan to find what nodes may be failing

---
+++ plans/check_online.pp
@@ -0,0 +1,34 @@
+# Checks each node to see if Puppet is installed, if it is then gather Facts
+plan patching::check_online (
+  TargetSpec $nodes,
+) {
+  $targets = get_targets($nodes)
+  ## This will check all nodes to verify online by checking their Puppet agent version
+  $targets_version = run_task('puppet_agent::version', $targets,
+                              _catch_errors => true)
+  # if we're filtering out offline nodes, then only accept the ok_set from the task above
+  if !$targets_version.error_set.empty() {
+    $errors_array = Array($targets_version.error_set)
+    $sorted_errors = $errors_array.sort|$a, $b| {
+      compare($a.target.name, $b.target.name)
+    }
+    out::message('###################################')
+    out::message('List of targets that failed to connect')
+    $sorted_errors.each |$res| {
+      out::message("- ${name}")
+    }
+    out::message('###################################')
+    out::message('Details for each failed target')
+    $sorted_errors.each |$res| {
+      $name = $res.target.name
+      $issue_code = $res.error.issue_code
+      $msg = $res.error.msg
+      out::message("- name: ${name}")
+      out::message("  error: [${issue_code}] ${msg}")
+    }
+    fail_plan('Unable to connect to the targets above!')
+  }
+  else {
+    out::message("All nodes succeeded!")
+  }
+}
