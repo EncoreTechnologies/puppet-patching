8d8ea2b0b833e9673de0699cdaa78dd3cef48321
Nick Maludy <nmaludy@gmail.com>
2019-09-10 14:59:35 -0400

    fixing patching results problems

---
+++ WINDOWS_UPDATE_PROBLEMS_AND_RESOLUTIONS.md
@@ -0,0 +1,16 @@
+# Windows update HRESULT problems and fixes
+
+- hresult: 0x8024402C 
+  possibilities:
+    - problem: a proxy setting in the system that is broken
+      solutions:
+       - fix the proxy config by going to IE -> Internet Options -> Connections
+       - either set proxy to "auto detect" or fix the proxy server
+    - problem: DNS is broken on the server and can't ping / access the WSUS server by name
+      troubleshooting: try to ping the WSUS server by hostname
+      solutions: 
+        - fix DNS so you can ping the WSUS server
+        - could be bad DNS on the NIC
+        - could be bad firewall policy on the host
+        - could be bad firewall policy on the network
+      
--- tasks/cache_remove.ps1
+++ tasks/cache_remove.ps1
@@ -7,14 +7,19 @@ $ProgressPreference = 'SilentlyContinue'
 # https://gallery.technet.microsoft.com/scriptcenter/Reset-WindowsUpdateps1-e0c5eb78
 
 # Stop the Windows Update service
+Write-Host "Stopping service... bits"
 Stop-Service -Name bits
+Write-Host "Stopping service... wuauserv"
 Stop-Service -Name wuauserv
 
 # Remove the downloaded updates
+Write-Host "Removing downloaded updates... $env:systemroot\SoftwareDistribution\Download"
 Remove-Item "$env:systemroot\SoftwareDistribution\Download" -force -Confirm:$false -Recurse  -ErrorAction SilentlyContinue
+Write-Host "Removing downloaded updates... $env:allusersprofile\Application Data\Microsoft\Network\Downloader\qmgr*.dat"
 Remove-Item "$env:allusersprofile\Application Data\Microsoft\Network\Downloader\qmgr*.dat" -force -Confirm:$false -ErrorAction SilentlyContinue 
 
 # Re-register all WSUS DLLs
+Write-Host "Re-registering WSUS DLLs"
 Set-Location $env:systemroot\system32 
 regsvr32.exe /s atl.dll 
 regsvr32.exe /s urlmon.dll 
@@ -54,10 +59,13 @@ regsvr32.exe /s muweb.dll
 regsvr32.exe /s wuwebv.dll 
 
 # Start the Windows Update service
+Write-Host "Starting service... bits"
 Start-Service -Name bits
+Write-Host "Starting service... wuauserv"
 Start-Service -Name wuauserv
 
 # Force WSUS discovery
+Write-Host "Forcing WSUS re-auth and discovery..."
 wuauclt /resetauthorization /detectnow
 
 exit 0
