49e59c166fe2dd7dc0cb2701d45c6a975d70ac7f
Nick Maludy <nmaludy@gmail.com>
2020-02-25 11:16:29 -0500

    Initial work on converting over to Bolt2

--- plans/pre_post_update.pp
+++ plans/pre_post_update.pp
@@ -3,36 +3,36 @@
 # @see patching::pre_update
 # @see patching::post_update
 #
-# @param [TargetSpec] nodes
+# @param [TargetSpec] targets
 #   Set of targets to run against.
 #
 # @param [String[1]] task
 #   Name of the pre/post update task to execute.
 #
 # @param [Optional[String[1]]] script_linux
-#   Path to the script that will be executed on Linux nodes.
+#   Path to the script that will be executed on Linux targets.
 #
 # @param [Optional[String[1]]] script_windows
-#   Path to the script that will be executed on Windows nodes.
+#   Path to the script that will be executed on Windows targets.
 #
 # @param [Boolean] noop
 #   Flag to enable noop mode for the underlying plans and tasks.
 #
 # @return [ResultSet] Returns the ResultSet from the execution of `task`.
 plan patching::pre_post_update (
-  TargetSpec $nodes,
+  TargetSpec $targets,
   String[1] $task,
   Optional[String[1]] $script_linux   = undef,
   Optional[String[1]] $script_windows = undef,
   Boolean             $noop           = false,
 ) {
   out::message("pre_post_update - noop = ${noop}")
-  $targets = run_plan('patching::get_targets',
-                      nodes => $nodes)
+  $_targets = run_plan('patching::get_targets',
+                        targets => $targets)
 
   # split into linux vs Windows
-  $targets_linux = $targets.filter |$t| { facts($t)['os']['family'] != 'windows' }
-  $targets_windows = $targets.filter |$t| { facts($t)['os']['family'] == 'windows' }
+  $targets_linux = $_targets.filter |$t| { facts($t)['os']['family'] != 'windows' }
+  $targets_windows = $_targets.filter |$t| { facts($t)['os']['family'] == 'windows' }
 
   # run pre-patch scripts
   if !$targets_linux.empty() {
--- plans/pre_update.pp
+++ plans/pre_update.pp
@@ -20,12 +20,12 @@
 #   patching_pre_update_script_linux: /usr/local/bin/mysweetpatchingscript.sh
 #
 # groups:
-#   # these nodes will use the pre patching script defined in the vars above
+#   # these targets will use the pre patching script defined in the vars above
 #   - name: regular_nodes
 #     targets:
 #       - tomcat01.domain.tld
 #
-#   # these nodes will use the customized patching script set for this group
+#   # these targets will use the customized patching script set for this group
 #   - name: sql_nodes
 #     vars:
 #       patching_pre_update_script_linux: /bin/sqlpatching.sh
@@ -33,12 +33,12 @@
 #       - sql01.domain.tld
 # ```
 #
-# @param [TargetSpec] nodes
+# @param [TargetSpec] targets
 #   Set of targets to run against.
 # @param [String[1]] script_linux
-#   Path to the script that will be executed on Linux nodes.
+#   Path to the script that will be executed on Linux targets.
 # @param [String[1]] script_windows
-#   Path to the script that will be executed on Windows nodes.
+#   Path to the script that will be executed on Windows targets.
 # @param [Boolean] noop
 #   Flag to enable noop mode for the underlying plans and tasks.
 #
@@ -46,34 +46,34 @@
 #   Returns the ResultSet from the underlying `run_task('patching::pre_update')`
 #
 # @example CLI - Basic usage
-#   bolt plan run patching::pre_update --nodes all_hosts
+#   bolt plan run patching::pre_update --targets all_hosts
 #
 # @example CLI - Custom scripts
-#   bolt plan run patching::pre_update --nodes all_hosts script_linux='/my/sweet/script.sh' script_windows='C:\my\sweet\script.ps1'
+#   bolt plan run patching::pre_update --targets all_hosts script_linux='/my/sweet/script.sh' script_windows='C:\my\sweet\script.ps1'
 #
 # @example Plan - Basic usage
 #   run_plan('patching::pre_update',
-#            nodes => $all_hosts)
+#            targets => $all_hosts)
 #
 # @example Plan - Custom scripts
 #   run_plan('patching::pre_update',
-#            nodes          => $all_hosts,
+#            targets        => $all_hosts,
 #            script_linux   => '/my/sweet/script.sh',
 #            script_windows => 'C:\my\sweet\script.ps1')
 #
 plan patching::pre_update (
-  TargetSpec $nodes,
+  TargetSpec $targets,
   String[1] $script_linux   = '/opt/patching/bin/pre_update.sh',
   String[1] $script_windows = 'C:\ProgramData\patching\bin\pre_update.ps1',
   Boolean   $noop           = false,
 ) {
-  $targets = run_plan('patching::get_targets', nodes => $nodes)
-  $group_vars = $targets[0].vars
+  $_targets = run_plan('patching::get_targets', targets => $targets)
+  $group_vars = $_targets[0].vars
   $_script_linux = pick($group_vars['patching_pre_update_script_linux'], $script_linux)
   $_script_windows = pick($group_vars['patching_pre_update_script_windows'], $script_windows)
 
   return run_plan('patching::pre_post_update',
-                  nodes          => $targets,
+                  targets        => $_targets,
                   task           => 'patching::pre_update',
                   script_linux   => $_script_linux,
                   script_windows => $_script_windows,
